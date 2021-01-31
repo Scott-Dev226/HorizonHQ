f0a8f8e8ac95de7fdb20276f48f0616d0fa30383
ScottDev226 <scottni219@gmail.com>
2021-01-30 21:00:18 -0500

    bug review

--- src/Components/App.js
+++ src/Components/App.js
@@ -39,6 +39,7 @@ function App() {
             <Route
               exact
               path="/"
+              exact
               render={() => <Home darkModeProp={isDarkMode} />}
             />
             <Route path="/Guitars" component={Guitars} />
